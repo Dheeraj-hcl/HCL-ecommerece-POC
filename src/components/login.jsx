f46fafa50537cd9a20946cb58e99242708c37d41
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-11-22 16:27:34 +0530

    add hooks and useEffect for validation to login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,24 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Login(props) {
   const [isNew, setIsNew] = useState(props.isNewForLogin);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   function changeIsNew() {
     setIsNew();
     props.changeIsNewForLogin(true);
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  }
+
+  function validate(values) {
+    const errors = {};
+    // const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.email) {
+      errors.email = "Email is required!";
+     }
+    //  else if( !regex.test(values.email)){
+    //   errors.email = "Please enter a valid email";
+    // }
+    if (!values.password) {
+      errors.password = "Password is required";
+    }
+    return errors;
+  }
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="from-inner">
           <div className="form-group">
-            <label htmlFor="email">Username</label>
-            <input placeholder="enter your username" type="email" />
+            <label htmlFor="email">Email:</label>
+            <input
+              name="email"
+              placeholder="enter your Email"
+              type="email"
+              onChange={handleChange}
+              value={formValues.email}
+            />
+            <p>{formErrors.email}</p>
           </div>
           <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input placeholder="enter your password" type="password" />
+            <label htmlFor="password">Password:</label>
+            <input
+              name="password"
+              placeholder="enter your password"
+              type="password"
+              onChange={handleChange}
+              value={formValues.password}
+            />
+            <p>{formErrors.password}</p>
           </div>
           <button type="submit">Login in</button>
           <p>New Here?</p>
