c253cd3d079fd8efa7c7ae3b0367b67c32149945
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-11-30 18:04:39 +0530

    modify user component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,8 @@
 import { useQuery, gql } from "@apollo/client";
-
+const $id = "user-0001"
 const GET_USER = gql`
-  query GetUser() {
-    userProfile(id: "user-0001") {
+  query getUser($id: ID!){
+    userProfile(id: $id) {
       id
       fullName
       phoneNumber
@@ -11,19 +11,22 @@ const GET_USER = gql`
   }
 `;
 
-function DisplayUser() {
-  const { loading, error, data } = useQuery(GET_USER);
+function DisplayUser({id}) {
+  const { loading, error, data } = useQuery(GET_USER,{
+    variables: {id: "user-0001"}
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
-
-  return data.users.map(({ id, fullName, phoneNumber, email }) => (
-    <div key={id}>
+  const {id: userId,fullName,phoneNumber,email} = data.userProfile;
+    console.log(data.userProfile)
+  return ( 
+    <div key={userId}>
       <h3>Full Name: {fullName}</h3>
       <p>Phone Number: {phoneNumber}</p>
       <p>Email: {email}</p>
     </div>
-  ));
+  );
 }
 
 export default DisplayUser;
