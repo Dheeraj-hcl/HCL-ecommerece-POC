ac0bd805ea03861e3900fcca7a5bd31462db5017
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-11-30 15:12:30 +0530

    Add User component to Home for testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import DisplayUser from "./User";
 function Home(){
+    const [isUserProfile, setIsUserProfile] = useState(false)
+    function handleClick(){
+        setIsUserProfile(true);
+    }
     return(
         <div>
             <Header />
+            <button onClick={handleClick}>my account</button>
+            {isUserProfile ? <DisplayUser />: null}
         </div>
     )
 }
