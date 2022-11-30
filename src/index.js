87ec51fff5cdf04265d924886616e87c865183ca
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-11-30 15:11:04 +0530

    add appolo connection to graphQl

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import App from "./components/App";
+
+const client = new ApolloClient({
+  uri: "localhost:9000/graphql",
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
