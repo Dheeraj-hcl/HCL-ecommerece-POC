b711daaff6af4ae47b2b2d2ae9ea4cd864c830e9
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-12-05 10:46:24 +0530

    add semantic UI form, useMutation for register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,42 +1,128 @@
 import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import { useMutation, gql } from "@apollo/client";
+
+const REGISTER_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $phoneNumber: String!
+    $email: String!
+    $password: String!
+  ) {
+    userRegister(
+      firstName: $firstName
+      lastName: $lastName
+      phoneNumber: $phoneNumber
+      email: $email
+      password: $password
+    ) {
+      id
+      fullName
+      email
+      phoneNumber
+    }
+  }
+`;
 
 function Register(props) {
   const [isNew, setIsNew] = useState(props.isNewForReg);
+  const [errors, setErrors] = useState({});
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+  });
 
   function changeIsNew() {
     setIsNew(false);
     props.changeIsNewForReg(false);
   }
 
+  function onChange(event) {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  }
+
+  const [addUser, { loading, error, data }] = useMutation(REGISTER_USER, {
+    update(proxy, result) {
+      console.log(result);
+    },
+    onError({ graphQLErrors }) {
+      setErrors(graphQLErrors[0].errors);
+    },
+    variables: values,
+  });
+
+  function onSubmit(event) {
+    event.preventDefault();
+    addUser();
+  }
+
   return (
-    <div>
-      <form>
-        <div className="from-inner">
-          <div className="form-group">
-            <label htmlFor="name">FirstName</label>
-            <input placeholder="enter your firstname" type="text" />
-          </div>
-          <div className="form-group">
-            <label>LastName</label>
-            <input placeholder="enter your lastname" type="text" />
-          </div>
-          <div className="form-group">
-            <label>PhoneNumber</label>
-            <input placeholder="enter your Phone Number" type="text" />
-          </div>
-          <div className="form-group">
-            <label>Username</label>
-            <input placeholder="enter your email" type="email" />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input placeholder="enter your password" type="password" />
-          </div>
-          <button type="submit">Sign Up</button>
+    <div className="form-container">
+      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
+        <div>
+          <Form.Input
+            label="First Name"
+            placeholder="First Name"
+            name="firstName"
+            type="text"
+            value={values.firstName}
+            error={errors.firstName ? true : false}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Last Name"
+            placeholder="Last Name"
+            name="lastName"
+            type="text"
+            value={values.lastName}
+            error={errors.lastName ? true : false}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Phone Number"
+            placeholder="Phone Number"
+            name="phoneNumber"
+            type="text"
+            value={values.phoneNumber}
+            error={errors.phoneNumber ? true : false}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Email"
+            placeholder="Email"
+            name="email"
+            type="email"
+            value={values.email}
+            error={errors.email ? true : false}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Password"
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={values.password}
+            error={errors.password ? true : false}
+            onChange={onChange}
+          />
+          <Button type="submit">Sign Up</Button>
+          {Object.keys(errors).length > 0 && (
+            <div >
+              <ul >
+                {Object.values(errors).map((value) => (
+                  <li key={value}>{value}</li>
+                ))}
+              </ul>
+            </div>
+          )}
           <p>Already have an account?</p>
           <p onClick={changeIsNew}>Login</p>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
