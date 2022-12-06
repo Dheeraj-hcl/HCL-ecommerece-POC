ab5aee5d0e9f33e369d67478bfb31f48fbf5ef74
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-12-06 15:26:13 +0530

    add header 2

---
+++ src/components/Header2.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+
+export default class MenuExampleHeader extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu>
+        <Menu.Item
+          header
+          icon="bars"
+          name="All Categories"
+          active={activeItem === "All Categories"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="Super Deals"
+          active={activeItem === "Super Deals"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="Featured Brands"
+          active={activeItem === "Featured Brands"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="Collections"
+          active={activeItem === "Collections"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="Best Selling"
+          active={activeItem === "Best Selling"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="Buy Again"
+          active={activeItem === "Buy Again"}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name="Customer Service"
+          active={activeItem === "Customer Service"}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
+    );
+  }
+}
