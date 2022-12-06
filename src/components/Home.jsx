1be812aef79f183b6fcd487875685ed01b0bff34
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-12-06 10:43:33 +0530

    add use location

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./Header";
 import DisplayUser from "./User";
 function Home(){
     const [isUserProfile, setIsUserProfile] = useState(false)
+    const location = useLocation();
     function handleClick(){
         setIsUserProfile(true);
     }
     return(
         <div>
             <Header />
+            <h1>Hi {location.state.fullName}</h1>
             <button onClick={handleClick}>my account</button>
-            {isUserProfile ? <DisplayUser />: null}
+            {isUserProfile ? <DisplayUser id={location.state.id}/>: null}
         </div>
     )
 }
