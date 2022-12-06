aa8de545cccff858930ef154fbab43b9f7d009e4
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-12-06 10:49:45 +0530

    add useLocation and link to login page

--- src/components/SuccessRegister.jsx
+++ src/components/SuccessRegister.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function SuccessRegister(props) {
-  return <h1>You have successfully singned up1</h1>;
+  const location = useLocation();
+  return (
+  <div>
+    <h1>Hi {location.state}! You have successfully signed up</h1>
+  <h1>click {<Link to='/'>here</Link>} to go to login page</h1>
+  </div>
+  );
 }
 
 export default SuccessRegister;
