c8e684efcd7569213d03e36aa24f793b8d21b741
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2022-12-07 12:58:50 +0530

    add un treacked files

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UncontrolledExample from './Carousel';
+const Content = () => {
+    return (
+        <div>
+            <UncontrolledExample />
+        </div>
+    );
+};
+
+export default Content;
